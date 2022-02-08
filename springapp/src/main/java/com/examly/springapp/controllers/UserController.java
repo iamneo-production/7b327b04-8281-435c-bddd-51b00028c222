cae7624111525483742585428d32006d4f289fc0
srinjoy0810 <srinjoy2468@gmail.com>
2022-02-08 13:15:32 +0530

    updated the user management

--- springapp/src/main/java/com/examly/springapp/controllers/UserController.java
+++ springapp/src/main/java/com/examly/springapp/controllers/UserController.java
@@ -9,40 +9,42 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.examly.springapp.database.entities.User;
 import com.examly.springapp.database.repositories.UserRepo;
 import com.examly.springapp.services.userServ;
+import com.examly.springapp.models.UserModel;
 
 @RestController
 //@CrossOrigin(origins="*", allowedHeaders="*")
 public class UserController {
 	
-//	@Autowired
-//	public userServ us;
-//	@Autowired
-//	public UserRepo ur;
-//	@RequestMapping("/home")
-//	public User hello(Authentication authentication){
-//		return ur.findByEmail(authentication.getName()).get();
-//	}
-//
-//	@RequestMapping(value="/admin", method = RequestMethod.GET)
-//	public List<User> getUser(){
-//		return us.getUser();
-//	}
-//	@RequestMapping(method=RequestMethod.PUT, value="/admin/userEdit/{id}")
-//	ResponseEntity<?> userEdit(@RequestBody User user, @PathVariable String id) {
-//		//userEditSave(us.userEdit(id));
-//		return us.userEdit(user,id);
-//	}
-//	@RequestMapping(method=RequestMethod.POST, value="/admin/addUser")
-//	ResponseEntity<?> userEditSave(@RequestBody User user){
-//		return us.userEditSave(user);
-//	}
-//	@RequestMapping(method=RequestMethod.DELETE, value="/admin/delete/{id}")
-//	ResponseEntity<?> userDelete(@PathVariable String id) {
-//		//userEditSave(us.userEdit(id));
-//		return us.userDelete(id);
-//	}
+	@Autowired
+	public userServ us;
+	@Autowired
+	public UserRepo ur;
+	// @RequestMapping("/home")
+	// public User hello(Authentication authentication){
+	// 	return ur.findByEmail(authentication.getName()).get();
+	// }
+
+	@RequestMapping(value="/admin", method = RequestMethod.GET)
+	public List<User> getUser(){
+		return us.getUser();
+	}
+	// @RequestMapping(method=RequestMethod.PUT, value="/admin/userEdit/{id}")
+	// ResponseEntity<?> userEdit(@ModelAttribute UserModel userModel, @PathVariable String id) {
+	// 	//userEditSave(us.userEdit(id));
+	// 	return us.userEdit(userModel,id);
+	// }
+	//@RequestMapping(method=RequestMethod.POST, value="/admin/addUser")
+	//ResponseEntity<?> userEditSave(@RequestBody User user){
+		//return us.userEditSave(user);
+	//}
+	@RequestMapping(method=RequestMethod.DELETE, value="/admin/delete/{id}")
+	ResponseEntity<?> userDelete(@PathVariable String id) {
+		//userEditSave(us.userEdit(id));
+		return us.userDelete(id);
+	}
 }
--- springapp/src/main/java/com/examly/springapp/services/userServ.java
+++ springapp/src/main/java/com/examly/springapp/services/userServ.java
@@ -7,60 +7,69 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.examly.springapp.database.entities.User;
 import com.examly.springapp.database.repositories.UserRepo;
+import com.examly.springapp.models.UserModel;
+import com.examly.springapp.database.enums.Role;
+import java.time.LocalDate;
 
 @Service
 public class userServ {
-//	@Autowired
-//	public UserRepo ur;
-//
-//	public User user;
-//
-//	@Autowired
-//	public loginRepo lr;
-//
-//	public List<User> getUser(){
-//		List<User> user=new ArrayList<>();
-//		ur.findAll().forEach(user::add);
-//		return user;
-//	}
-//
-//	public List<User> getOnlineUser() {
-//		List<User> onlineuser=new ArrayList<>();
-//		ur.findByActive(true).forEach(onlineuser::add);
-//		return onlineuser;
-//	}
-//
-//	public ResponseEntity<?> userEditSave(User user) {
-//		HashMap<String, String> h=new HashMap<>();
-//		ur.save(user);
-//		login l=new login(user.getEmail(),user.getPassword());
-//		lr.save(l);
-//		h.put("Message", "User added successfully");
-//		return ResponseEntity.ok(h);
-//	}
-//
-//	public ResponseEntity<?> userEdit(User user, String id) {
-//		ur.save(user);
-//		login l=new login(user.getEmail(),user.getPassword());
-//		lr.save(l);
-//		HashMap<String, String> h=new HashMap<>();
-//		h.put("Message", "User added successfully");
-//		return ResponseEntity.ok(h);
-//	}
-//
-//	public ResponseEntity<?> userDelete(String id) {
-//		//lr.deleteById(ur.findById(id).get().getEmail());
-//		ur.deleteById(id);
-//		HashMap<String, String> h=new HashMap<>();
-//		h.put("Message", "User deleted successfully");
-//		return ResponseEntity.ok(h);
-//	}
-//
-//	public User getUser(String id) {
-//		return ur.findById(id).orElse(null);
-//	}
-//
+	@Autowired
+	public UserRepo ur;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+	public User user;
+
+
+	public List<User> getUser(){
+		List<User> user=new ArrayList<>();
+		ur.findAll().forEach(user::add);
+		return user;
+	}
+
+	// public List<User> getOnlineUser() {
+	// 	List<User> onlineuser=new ArrayList<>();
+	// 	ur.findByActive(true).forEach(onlineuser::add);
+	// 	return onlineuser;
+	// }
+
+	// public ResponseEntity<?> userEditSave(User user) {
+	// 	HashMap<String, String> h=new HashMap<>();
+	// 	ur.save(user);
+	// 	login l=new login(user.getEmail(),user.getPassword());
+	// 	lr.save(l);
+	// 	h.put("Message", "User added successfully");
+	// 	return ResponseEntity.ok(h);
+	// }
+
+	// public ResponseEntity<?> userEdit(UserModel userModel, String id) {
+	// 	ur.save(new User(
+    //         userModel.getUsername(),
+    //         userModel.getEmail(),
+    //         passwordEncoder.encode(userModel.getPassword()),
+    //         userModel.getMobileNumber(),
+    //         true,
+    //         Role.CUSTOMER,
+    //         LocalDate.now()
+    //     ));
+	// 	HashMap<String, String> h=new HashMap<>();
+	// 	h.put("Message", "User edited successfully");
+	// 	return ResponseEntity.ok(h);
+	// }
+
+	public ResponseEntity<?> userDelete(String id) {
+		//lr.deleteById(ur.findById(id).get().getEmail());
+		ur.deleteById(id);
+		HashMap<String, String> h=new HashMap<>();
+		h.put("Message", "User deleted successfully");
+		return ResponseEntity.ok(h);
+	}
+
+	public User getUser(String id) {
+		return ur.findById(id).orElse(null);
+	}
+
 }
