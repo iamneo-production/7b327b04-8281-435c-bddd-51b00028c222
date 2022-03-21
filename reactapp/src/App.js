0897be729117dd192210f44cb524d0937442b171
Sarthak Mondal <raceison139@gmail.com>
2022-03-21 20:38:20 +0530

    New Frontend Pushed

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,36 +1,90 @@
-import React, { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Header from "./components/Navbar/navbar";
-import "bootstrap/dist/css/bootstrap.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import HomePage from "./components/HomePage/homePage";
-import { connect } from "react-redux";
-import actionTypes from "./store/actions/actionTypes";
-import UsersContainer from "./containers/Admin/Users/users";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./General/Home/Home";
+import About from "./General/About/About";
+import Contact from "./General/Contact/Contact";
+import Login from "./General/Login/Login";
+import ResetPassword from "./General/Login/ResetPassword";
+import Profile from "./General/Profile/Profile";
+import Signup from "./UserUI/Signup/Signup";
+import UpdateProfile from "./General/UpdateProfile/UpdateProfile";
+import AddTheme from "./AdminUI/AddTheme/AddTheme";
+import UpdateTheme from "./AdminUI/UpdateTheme/UpdateTheme";
+import ManageTheme from "./AdminUI/ManageTheme/ManageTheme";
+import AddFood from "./AdminUI/AddFood/AddFood";
+import ManageFood from "./AdminUI/ManageFood/ManageFood";
+import UpdateFood from "./AdminUI/UpdateFood/UpdateFood";
+import AddAddon from "./AdminUI/AddAddon/AddAddon";
+import ManageAddon from "./AdminUI/ManageAddon/ManageAddon";
+import UpdateAddon from "./AdminUI/UpdateAddon/UpdateAddon";
+import UpdateUsers from "./AdminUI/UpdateUser/UpdateUser";
+import ManageUsers from "./AdminUI/ManageUsers/ManageUsers";
+import AddBankDetails from "./UserUI/AddBankDetails/AddBankDetails";
+import ViewBankDetails from "./UserUI/ViewBankDetails/ViewBankDetails";
+import ViewThemes from "./UserUI/ViewThemes/ViewThemes";
+import ViewFoods from "./UserUI/ViewFoods/ViewFoods";
+import ViewAddons from "./UserUI/ViewAddons/ViewAddon";
+import AddEvent from "./UserUI/AddEvent/AddEvent";
+import EventDetails from "./UserUI/EventDetails/EventDetails"
+import InvitationCard from "./General/InvitationCard/InvitationCard";
+import MyEvents from "./UserUI/MyEvents/MyEvents";
+import UnAuthorized from "./General/Errors/401";
+import Forbidden from "./General/Errors/403";
+import NotFound from "./General/Errors/404";
 
 function App() {
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (user) {
-      //props.setUser(user);
-    } else {
-      //window.location.href = "/";
-    }
-  }, []);
   return (
-    <div className="App">
-      <BrowserRouter>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route exact path="/admin/users" component={UsersContainer} />
-            <Route exact path="/" component={HomePage} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-      <ToastContainer />
+    <div>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home}></Route>
+          <Route exact path='/about' component={About}></Route>
+          <Route exact path='/contact' component={Contact}></Route>
+          <Route exact path='/login' component={Login}></Route>
+          <Route exact path='/resetpassword' component={ResetPassword}></Route>
+          <Route exact path='/signup' component={Signup}></Route>
+          <Route exact path='/profile' component={Profile}></Route>
+          <Route exact path='/updateprofile/:uId' component={UpdateProfile}></Route>
+
+          <Route exact path='/addtheme' component={AddTheme}></Route>
+          <Route exact path='/updatetheme/:tId' component={UpdateTheme}></Route>
+          <Route exact path='/managetheme' component={ManageTheme}></Route>
+
+          <Route exact path='/addfood' component={AddFood}></Route>
+          <Route exact path='/updatefood/:fId' component={UpdateFood}></Route>
+          <Route exact path='/managefood' component={ManageFood}></Route>
+
+          <Route exact path='/addaddon' component={AddAddon}></Route>
+          <Route exact path='/updateaddon/:aId' component={UpdateAddon}></Route>
+          <Route exact path='/manageaddon' component={ManageAddon}></Route>
+
+          <Route exact path='/updateuser/:uId' component={UpdateUsers}></Route>
+          <Route exact path='/manageusers' component={ManageUsers}></Route>
+
+          <Route exact path='/viewthemes' component={ViewThemes}></Route>
+          <Route exact path='/viewfoods' component={ViewFoods}></Route>
+          <Route exact path='/viewaddons' component={ViewAddons}></Route>
+
+          <Route exact path='/addevent' component={AddEvent}></Route>
+          <Route exact path='/myevents' component={MyEvents}></Route>
+          <Route exact path='/viewcard' component={InvitationCard}></Route>
+
+          <Route exact path='/addbankdetails' component={AddBankDetails}></Route>
+          <Route exact path='/viewbankdetails' component={ViewBankDetails}></Route>
+          <Route exact path='/eventdetails/:eId' component={EventDetails}></Route>
+
+          <Route exact path='/unauthorized' component={UnAuthorized}></Route>
+          <Route exact path='/forbidden' component={Forbidden}></Route>
+          <Route component={NotFound}></Route>
+
+        </Switch>
+      </Router>
+      <Footer />
+
     </div>
   );
 }
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,34 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import userReducer from "./store/reducers/user";
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const rootReducer = combineReducers({
-  user: userReducer,
-});
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
