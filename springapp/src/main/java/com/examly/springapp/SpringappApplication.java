954febc05c5d0c29765ec1e7f8c6a5ec38030212
Rajtilak Pal <rajtilakls2510@gmail.com>
2022-03-26 11:53:49 +0530

    Added Event Fetch By Ids and By User Id

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,14 +1,9 @@
 package com.examly.springapp;
 
-import com.examly.springapp.database.entities.AddOn;
-import com.examly.springapp.database.entities.Menu;
-import com.examly.springapp.database.entities.Theme;
-import com.examly.springapp.database.entities.User;
+import com.examly.springapp.database.entities.*;
+import com.examly.springapp.database.enums.EventState;
 import com.examly.springapp.database.enums.Role;
-import com.examly.springapp.database.repositories.AddOnRepo;
-import com.examly.springapp.database.repositories.MenuRepo;
-import com.examly.springapp.database.repositories.ThemeRepo;
-import com.examly.springapp.database.repositories.UserRepo;
+import com.examly.springapp.database.repositories.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -34,7 +30,7 @@ public class SpringappApplication {
     }
 
     @Bean
-    CommandLineRunner runner(UserRepo userRepo, PasswordEncoder passwordEncoder, ThemeRepo themeRepo, MenuRepo menuRepo, AddOnRepo addOnRepo) {
+    CommandLineRunner runner(UserRepo userRepo, PasswordEncoder passwordEncoder, ThemeRepo themeRepo, MenuRepo menuRepo, AddOnRepo addOnRepo, EventRepo eventRepo) {
         return args -> {
             Optional<User> byEmail = userRepo.findByEmail("admin@mail.com");
             User rootAdmin = null;
@@ -47,7 +43,18 @@ public class SpringappApplication {
             }
             userRepo.save(rootAdmin);
 
+            // Adding demo customers
+            List<User> users = new ArrayList<>();
+            for(int i=0;i<5;i++)
+            {
+                User user = new User("customer"+i, "customer"+i+"@mail.com", passwordEncoder.encode("customer"), null, true, Role.CUSTOMER, LocalDate.now());
+                userRepo.save(user);
+                users.add(user);
+            }
+
+
             // Adding Themes
+            List<Theme> themes = new ArrayList<>();
             for (int i = 0; i < 5; i++) {
                 Theme theme = new Theme(
                         "Theme "+i,
@@ -61,8 +68,10 @@ public class SpringappApplication {
                         rootAdmin
                 );
                 themeRepo.save(theme);
+                themes.add(theme);
             }
             // Adding Menus
+            List<Menu> menus = new ArrayList<>();
             for(int i=0;i<5;i++){
                 Menu menu = new Menu(
                         "Some type"+i,
@@ -72,8 +81,10 @@ public class SpringappApplication {
                         rootAdmin
                 );
                 menuRepo.save(menu);
+                menus.add(menu);
             }
             // Adding AddOns
+            List<AddOn> addOns = new ArrayList<>();
             for(int i=0;i<5;i++)
             {
                 AddOn addOn = new AddOn(
@@ -84,9 +95,40 @@ public class SpringappApplication {
                         rootAdmin
                 );
                 addOnRepo.save(addOn);
+                addOns.add(addOn);
+            }
+
+            // Adding Some Events
+
+            List<Event> events = new ArrayList<>();
+            for(int i=0;i<5;i++)
+            {
+                List<AddOn> addOnList = List.of(addOns.get((int) (Math.random()*5)),
+                        addOns.get((int) (Math.random()*5)));
+                Event event = new Event(
+                        "event"+i,
+                        "Some Applicant",
+                        "sdf",
+                        ":dfg",
+                        "sdfsdf",
+                        "sdfg",
+                        LocalDate.now(),
+                        LocalTime.now(),
+                        themes.get((int) (Math.random()*5)),
+                        menus.get((int) (Math.random()*5)),
+                        200D,
+                        LocalDate.now(),
+                        users.get((int) (Math.random()*5)),
+                        addOnList,
+                        EventState.BOOKED
+
+                );
+                eventRepo.save(event);
+                events.add(event);
             }
 
         };
+
     }
 
 }
--- springapp/src/main/java/com/examly/springapp/controllers/EventController.java
+++ springapp/src/main/java/com/examly/springapp/controllers/EventController.java
@@ -1,10 +1,7 @@
 package com.examly.springapp.controllers;
 
 import com.examly.springapp.database.entities.Event;
-import com.examly.springapp.exceptions.AddOnNotFoundException;
-import com.examly.springapp.exceptions.MenuNotFoundException;
-import com.examly.springapp.exceptions.ThemeNotFoundException;
-import com.examly.springapp.exceptions.UserNotFoundException;
+import com.examly.springapp.exceptions.*;
 import com.examly.springapp.models.EventModel;
 import com.examly.springapp.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,6 +45,104 @@ public class EventController {
             );
         });
         return new ResponseEntity<>(eventModelsResponse, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/admin/getEvent/{eventId}")
+    public ResponseEntity<?> getEventById(@PathVariable("eventId") String eventId) {
+        try {
+            Event event = eventService.getEventById(eventId);
+            List<String> addOnIds = new ArrayList<>();
+            event.getAddOns().forEach(addOn -> addOnIds.add(addOn.getAddOnId()));
+            EventModel eventModelResponse = new EventModel(
+                    event.getEventId(),
+                    event.getEventName(),
+                    event.getApplicantName(),
+                    event.getApplicantAddress(),
+                    event.getApplicantMobile(),
+                    event.getApplicantEmail(),
+                    event.getEventAddress(),
+                    event.getEventDate(),
+                    event.getEventTime(),
+                    event.getTheme().getThemeId(),
+                    event.getMenu().getMenuId(),
+                    event.getEventCost(),
+                    addOnIds,
+                    event.getState()
+            );
+            return new ResponseEntity<EventModel>(eventModelResponse, HttpStatus.OK);
+        } catch (EventNotFoundException e) {
+            return new ResponseEntity<String>("Event not found: " + eventId, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<String>("Something went wrong on our side. Please try again.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/user/getEvent/{eventId}")
+    public ResponseEntity<?> getEventByUserId(@PathVariable("eventId") String eventId, @RequestAttribute String user_id) {
+        try {
+            Event event = eventService.getEventById(eventId);
+            if(!event.getBookedBy().getId().equals(user_id))
+                return new ResponseEntity<String>("You are not allowed to view this event", HttpStatus.FORBIDDEN);
+            List<String> addOnIds = new ArrayList<>();
+            event.getAddOns().forEach(addOn -> addOnIds.add(addOn.getAddOnId()));
+            EventModel eventModelResponse = new EventModel(
+                    event.getEventId(),
+                    event.getEventName(),
+                    event.getApplicantName(),
+                    event.getApplicantAddress(),
+                    event.getApplicantMobile(),
+                    event.getApplicantEmail(),
+                    event.getEventAddress(),
+                    event.getEventDate(),
+                    event.getEventTime(),
+                    event.getTheme().getThemeId(),
+                    event.getMenu().getMenuId(),
+                    event.getEventCost(),
+                    addOnIds,
+                    event.getState()
+            );
+            return new ResponseEntity<EventModel>(eventModelResponse, HttpStatus.OK);
+        } catch (EventNotFoundException e) {
+            return new ResponseEntity<String>("Event not found: " + eventId, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<String>("Something went wrong on our side. Please try again.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/user/getEvents")
+    public ResponseEntity<?> getEventsOfUser(@RequestAttribute String user_id) {
+        try {
+            List<Event> events = eventService.getEventByUser(user_id);
+
+            List<EventModel> eventModelsResponse = new ArrayList<>();
+            events.forEach(event -> {
+                List<String> addOnIds = new ArrayList<>();
+                event.getAddOns().forEach(addOn -> addOnIds.add(addOn.getAddOnId()));
+                eventModelsResponse.add(
+                        new EventModel(
+                                event.getEventId(),
+                                event.getEventName(),
+                                event.getApplicantName(),
+                                event.getApplicantAddress(),
+                                event.getApplicantMobile(),
+                                event.getApplicantEmail(),
+                                event.getEventAddress(),
+                                event.getEventDate(),
+                                event.getEventTime(),
+                                event.getTheme().getThemeId(),
+                                event.getMenu().getMenuId(),
+                                event.getEventCost(),
+                                addOnIds,
+                                event.getState()
+                        )
+                );
+            });
+            return new ResponseEntity<List<EventModel>>(eventModelsResponse, HttpStatus.OK);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<String>("User not found: " + user_id, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<String>("Something went wrong on our side. Please try again.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping(path = "/user/bookEvent")
